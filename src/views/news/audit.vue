dca9e4acb118a2d2bfba3b90f8664446cef4d702
BacnNorman <87564734+BacnNorman@users.noreply.github.com>
2021-08-14 00:36:34 +0800

    Update audit.vue

--- src/views/news/audit.vue
+++ src/views/news/audit.vue
@@ -1,34 +1,159 @@
 <template>
-  <el-card shadow="hover">
-    <el-scrollbar height="300px">
-      <p class="item" v-for="item in reglist" :key="item">{{ item }}</p>
-    </el-scrollbar>
-    <el-pagination layout="prev, pager, next" :total="50"> </el-pagination
-  ></el-card>
-  <el-row></el-row>
-  <el-card shadow="hover">
-    <el-scrollbar height="300px">
-      <p class="item" v-for="item in newslist" :key="item">{{ item }}</p>
-    </el-scrollbar>
-    <el-pagination layout="prev, pager, next" :total="50"> </el-pagination>
-  </el-card>
+  <div class="news-list">
+    <el-row>
+      <el-col class="grid-content bg-purple-dark title">审核申请</el-col>
+    </el-row>
+    
+
+    <el-row
+      v-for="item in tableData"
+      :key="item.id"
+      @click="getExactly(item.id)"
+      class="news-item"
+    >
+      <div class="item">{{ item.title }}</div>
+      <span>{{ item.date }}</span>
+    </el-row>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[5, 10, 20, 40]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="totalItems"
+      :page-count="totalPages"
+      :background="true"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
+import { getNewsList } from "./api";
+import {searchtitle} from "./api";
 export default {
   data() {
-    const item1 = "这是注册审核列表";
-    const item2 = "这是资讯审核列表";
     return {
-      reglist: Array(20).fill(item1),
-      newslist: Array(20).fill(item2),
+      input:'',
+      currentPage: 1, //初始页
+      pagesize: 20, //    每页的数据
+      totalPages: 1,
+      totalItems: 0,
+      tableData: [],
     };
   },
-  created: () => {
-    console.log("created");
+  methods: {
+    getExactly(id) {
+      this.$router.push({ path: "/news/auditdetail", query: { id: id } });
+    },
+    search(input){
+      var that = this;
+   searchtitle(input).then(res=>{
+        console.log(res)
+        that.tableData = res.data;
+        that.currentPage = 1;
+        that.totalItems = res.data.length;
+        that.input='';
+      }
+    ).catch(err=>{
+      console.log(err)
+      if(err.error){
+        this.$message({
+          message: err.error,
+          type:"error"
+        })
+        this.$router.back()
+      }
+    }) 
+    },
+    handleSizeChange: function (size) {
+      console.log(this.pagesize); //每页下拉显示数据
+      this.pagesize = size;
+      this.loadNews("auditing",this.currentPage, size);
+    },
+    handleCurrentChange: function (currentPage) {
+      this.currentPage = currentPage;
+      this.loadNews("auditing",this.currentPage, this.pagesize);
+    },
+    loadNews(type,page, limit) {
+      getNewsList(type,page, limit).then(
+        (res) => {
+          const data = res.data;
+          this.tableData = data.records;
+          this.pagesize = this.totalItems = data.size;
+          this.totalPages = data.pages;
+        },
+        function (error) {}
+      );
+    },
   },
+  created() {
+    this.loadNews("auditing",1,20);
+  },
+
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
+
+.news-item {
+  justify-content: space-between;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+.item:hover {
+  color: rgb(99, 238, 248);
+  cursor: pointer;
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
+
+  color: rgb(219, 219, 231);
+}
+.bg-purple-dark {
+  background: #1353c9;
+}
+.bg-purple {
+  background: #3dce38;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 5px;
+  min-height: 36px;
+  color: #ffffff;
+}
+.grid-contents {
+  border-radius: 0px;
+  min-height: 36px;
+  color: #ffffff;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
 </style>
