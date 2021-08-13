d3eb0c5bc561bd03b32fdf6bfcf4e09e0ba3e4be
BacnNorman <87564734+BacnNorman@users.noreply.github.com>
2021-08-13 23:46:21 +0800

    Update audit.vue

--- src/views/user/audit.vue
+++ src/views/user/audit.vue
@@ -32,9 +32,7 @@
     v-model="dialogVisible"
     width="70%">
 
-  <el-descriptions class="margin-top" title="用户基本信息" :column="3" border>
-    <template #extra>
-    </template>
+  <el-descriptions class="margin-top" title="用户基本信息" :column="2" border>
     <el-descriptions-item>
       <template #label>
         <i class="el-icon-user"></i>
@@ -45,53 +43,139 @@
     <el-descriptions-item>
       <template #label>
         <i class="el-icon-mobile-phone"></i>
-        手机号
+        企业联系电话
       </template>
-      18100000000
+      {{detailform.comContact}}
     </el-descriptions-item>
     <el-descriptions-item>
       <template #label>
         <i class="el-icon-location-outline"></i>
-        居住地
-      </template>
-      苏州市
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template #label>
-        <i class="el-icon-tickets"></i>
-        备注
-      </template>
-      <el-tag size="small">学校</el-tag>
-    </el-descriptions-item>
-    <el-descriptions-item>
-      <template #label>
-        <i class="el-icon-office-building"></i>
         注册地址
       </template>
       {{detailform.comAddr}}
     </el-descriptions-item>
-  </el-descriptions>
-
-  <el-descriptions class="margin-top" title="相关资质查看" :column="3" border>
-    <template #extra>
-    </template>
-    <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-    <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-    <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
-    <el-descriptions-item label="备注">
-      <el-tag size="small">学校</el-tag>
+    <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-tickets"></i>
+        企业介绍
+      </template>
+      {{detailform.comIntro}}
     </el-descriptions-item>
-    <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
+     <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-office-building"></i>
+        邮政编码
+      </template>
+      {{detailform.comZip}}
+      </el-descriptions-item>
+       <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-office-building"></i>
+       传真
+      </template>
+      {{detailform.fax}}
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-office-building"></i>
+       注册资金
+      </template>
+      {{detailform.registeredCapital}}
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-location-outline"></i>
+        煤炭存放地点
+      </template>
+      {{detailform.coalStoreSite}}
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-office-building"></i>
+       煤炭数量
+      </template>
+      {{detailform.coalQuantity}}
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-office-building"></i>
+       煤炭质量
+      </template>
+      {{detailform.coalQulity}}
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <i class="el-icon-office-building"></i>
+       运输方式
+      </template>
+      {{detailform.coalTransport}}
+    </el-descriptions-item>
   </el-descriptions>
 
-
-
-
-
+  <el-descriptions class="margin-top" title="相关资质查看" :column="1" border>
+    <el-descriptions-item label="法人名称">{{detailform.legalName}}</el-descriptions-item>
+    <el-descriptions-item label="法人身份证号码">{{detailform.legalId}}</el-descriptions-item>
+    <el-descriptions-item label="法人身份证文件">
+  <el-upload
+  :file-list="fileList[0]"
+  action=""
+  @click="downloadpic(detailform.userId,detailform.legalIdFile,0)"
+ >
+</el-upload>
+  </el-descriptions-item>
+    <el-descriptions-item label="煤炭经营许可证编号">{{detailform.manageLicenseId}}</el-descriptions-item>
+    <el-descriptions-item label="煤炭经营许可证文件">
+<el-upload
+  :file-list="fileList[1]"
+  action=""
+  @click="downloadpic(detailform.userId,detailform.manageLicenseFile,0)"
+ >
+</el-upload>
+    </el-descriptions-item>
+    <el-descriptions-item label="企业营业执照号">{{detailform.businessLicenseId}}</el-descriptions-item>
+    <el-descriptions-item label="企业营业执照文件">
+<el-upload
+  :file-list="fileList[2]"
+  action=""
+  @click="downloadpic(detailform.userId,detailform.businessLicenseFile,0)"
+ >
+</el-upload>      
+    </el-descriptions-item>
+    <el-descriptions-item label="组织机构代码">{{detailform.oibCode}}</el-descriptions-item>
+    <el-descriptions-item label="组织机构代码证">
+<el-upload
+  :file-list="fileList[3]"
+  action=""
+  @click="downloadpic(detailform.userId,detailform.oibCodeFile,0)"
+ >
+</el-upload>
+    </el-descriptions-item>
+    <el-descriptions-item label="税务登记证代码">{{detailform.trCert}}</el-descriptions-item>
+    <el-descriptions-item label="税务登记证文件">
+<el-upload
+  :file-list="fileList[4]"
+  action=""
+  @click="downloadpic(detailform.userId,detailform.trCertFile,0)"
+ >
+</el-upload>
+    </el-descriptions-item>
+  </el-descriptions>
+    
+<h3>审核意见</h3>      
+<el-input
+  type="textarea"
+  placeholder="请输入内容"
+  v-model="auditopinion"
+  maxlength="100"
+  :rows="6"
+  show-word-limit
+>
+</el-input>
+    
        <template #footer>
       <span class="dialog-footer">
-      <el-button type="danger" @click="dialogVisible = false">拒绝</el-button>
-      <el-button type="success" @click="dialogVisible = false">通过</el-button>
+      <el-button type="danger" @click="pass(detailform.userId)">拒绝</el-button>
+      <el-button type="success" @click="notpass(detailform.userId)">通过</el-button>
       </span>
      </template>
     </el-dialog>
@@ -100,15 +184,24 @@
 </template>
 
 <script>
+import { h } from 'vue';
 import {getreglist} from "./api.js"
 import {getdetail} from "./api.js"
+import {downloadpicfile} from "./api.js"
+import {getpass} from "./api.js"
+import {getnotpass} from "./api.js"
+import {subauditopinion} from "./api.js"
 export default {
   data() {
     
     return {
+      fileList:[ [{name: '法人身份证.jpeg', url: ''}],
+                 [{name: '煤炭经营许可证.jpeg', url: ''}],
+                 [{name: '企业营业执照.jpeg', url: ''}],
+                 [{name: '组织机构代码证.jpeg', url: ''}],
+                 [{name: '税务登记证.jpeg', url: ''}]],
       detailform:{},
-      id:9,
-      enname:"",
+      auditopinion:"",
       reglist: [],
       dialogVisible: false,
       currentPage: 1, //初始页
@@ -124,7 +217,37 @@ export default {
     this.pagesize = size;
     this.loadreglist(this.currentPage, size);
     },
-
+    downloadpic(id,file,num){
+      
+       downloadpicfile(id,file).then(res=>{
+        console.log(res);
+        this.fileList[num].url=res.url;
+        this.$msgbox({
+          title: '详情',
+          message: h('div', null, [
+            h('h3', null, this.detailform.legalName),
+            h('img', { 
+              style:{ verticalAlign:"middle" },
+              attrs:{
+               src:require(this.fileList[num].url)
+              }
+            },)
+          ]),
+          confirmButtonText: '确定'
+        }).then();
+    
+      }
+    ).catch(err=>{
+      console.log(err)
+      if(err.error){
+        this.$message({
+          message: err.error,
+          type:"error"
+        })
+        this.$router.back()
+      }
+    }) 
+    },
     handleCurrentChange: function (currentPage) {
       this.currentPage = currentPage;
       this.loadreglist(this.currentPage, this.pagesize);
@@ -150,13 +273,27 @@ export default {
       }
     })
     },
+    pass(id){
+      this.dialogVisible = false
+      getpass(id)
+      subauditopinion(id,this.auditopinion)
+      },
+    notpass(id){
+      this.dialogVisible = false
+      getnotpass(id)
+      subauditopinion(id,this.auditopinion)
+      },
     geteadetail(id){
       var that = this;
     getdetail(id).then(res=>{
         console.log(res);
         console.log(res.data[0].legalName);
         that.detailform = res.data[0];
-        
+        that.fileList[0].name=that.detailform.legalIdFile
+        that.fileList[1].name=that.detailform.manageLicenseFile
+        that.fileList[2].name=that.detailform.businessLicenseFile
+        that.fileList[3].name=that.detailform.oibCode
+        that.fileList[4].name=that.detailform.trCertFile
       }
     ).catch(err=>{
       console.log(err)
